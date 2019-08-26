d670c455b5fe930c3db35b4edf47161ac5c9d27e
zeal yen <zealyen@noodoe.com>
2019-08-26 23:41:25 +0800

    update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/Lord28"));
+app.use(express.static(__dirname + "/dist"));
 
-console.log(path.join(__dirname + "/dist/Lord28/index.html"));
+// console.log(path.join(__dirname + "/dist/index.html"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/Lord28/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
